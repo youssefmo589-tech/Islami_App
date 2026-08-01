f14b8f65a2ab9727e651d2160c5eaac4130df917
youssefmo589-tech <youssefmo589@gmail.com>
2026-08-01 04:54:37 -0700

    updated UI for RadioPage

--- lib/modules/Radio/RadioCard.dart
+++ lib/modules/Radio/RadioCard.dart
@@ -3,26 +3,26 @@ import 'package:flutter/cupertino.dart';
 import '../../core/gen/assets.gen.dart';
 import '../../core/theme/AppColors.dart';
 
-class RadioCard extends StatefulWidget {
+class Radio_RecitersCard extends StatefulWidget {
   final String txt;
 
   bool isplayed;
 
   bool volumehigh;
 
-  RadioCard({
+  Radio_RecitersCard({
     super.key,
     required this.txt,
     this.isplayed = false,
     this.volumehigh = true,
   });
 
-  State<RadioCard> createState() {
+  State<Radio_RecitersCard> createState() {
     return _RadioCardState();
   }
 }
 
-class _RadioCardState extends State<RadioCard> {
+class _RadioCardState extends State<Radio_RecitersCard> {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
